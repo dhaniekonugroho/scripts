a31b897516347ed4acb6219de242e4ed1f2a4812
dhaniekonugroho <dhaniekonugroho@gmail.com>
2019-01-12 21:32:30 +0000

    Update install_qgis.sh

--- install_qgis.sh
+++ install_qgis.sh
@@ -2,4 +2,4 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key CAEB3DC3BDF7FB45
 echo "deb https://qgis.org/ubuntu cosmic main" | sudo tee -a /etc/apt/sources.list
 echo "deb-src https://qgis.org/ubuntu cosmic main" | sudo tee -a /etc/apt/sources.list
 sudo apt update
-sudo apt-get install qgis python-qgis qgis-plugin-grass
+sudo apt-get -y install qgis python-qgis qgis-plugin-grass
